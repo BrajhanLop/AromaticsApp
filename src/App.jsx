ee851f7353d60a0e1e05a272eee71022bd9752cf
EmilianoQuercia <emilianoquercia1@gmail.com>
2022-11-15 18:53:34 -0300

    cree las carpetas y mi componente (emi)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Main2 from "./components/main2/Main2"
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Aromatics App</h1>
+      <Main2/>
     </div>
   )
 }
---
+++ src/components/main2/Main2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main2 = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Main2;
