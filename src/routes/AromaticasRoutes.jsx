94427555376fdb68734abc6c231fa402836f3641
EmilianoQuercia <emilianoquercia1@gmail.com>
2022-11-22 20:02:48 -0300

    pagina index terminada desktop

--- src/routes/AromaticasRoutes.jsx
+++ src/routes/AromaticasRoutes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import ContainerFooter from "../components/footer/ContainerFooter";
+import NavbarApp from "../components/Navbar/Navbar-App";
 import Contacto from "../pages/contacto/Contacto";
 import Home from "../pages/home/Home";
 import Plantas from "../pages/plantas/Plantas";
@@ -8,14 +10,14 @@ import Plantas from "../pages/plantas/Plantas";
 const AromaticasRoutes = () => {
   return (
     <>
-      {/* Colocar aqui el navbar */}
+      <NavbarApp/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/plantas" element={<Plantas />} />
         <Route path="/contacto" element={<Contacto />} />
         {/* Agregar mas rutas */}
       </Routes>
-      {/* colocar aqui el footer */}
+      <ContainerFooter/>
     </>
   );
 };
