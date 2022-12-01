83b2b072b6259d296f764d5877db3eb4fb5c1662
EmilianoQuercia <emilianoquercia1@gmail.com>
2022-12-01 20:24:20 -0300

    complete la navegacion de plantas

--- src/components/Navbar/Navbar-App.jsx
+++ src/components/Navbar/Navbar-App.jsx
@@ -17,8 +17,7 @@ const NavbarApp = () => {
           <Navbar.Collapse id="basic-navbar-nav" className='navItem '>
             <Nav className="me-5">
               <Nav.Link className='item' href="/">HOME</Nav.Link>
-              <Nav.Link className='item' href="#link">CULTIVO</Nav.Link>
-              <Nav.Link className='item' href="#link">PLANTAS</Nav.Link>
+              <Nav.Link className='item' href="/plantas">PLANTAS</Nav.Link>
               <Nav.Link className='item' href="/contacto">CONTACTO</Nav.Link>
             </Nav>
           </Navbar.Collapse>
