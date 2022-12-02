8970c9727eefcf251a656aa7819950a0a1096cb2
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-12-02 00:37:19 -0500

    filtro input terminado

--- src/pages/plantas/detallesPlantas.jsx
+++ src/pages/plantas/detallesPlantas.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../components/styles/detallesPlantas.css'
 
 import Albahaca from '../../images/albahaca2.png'
@@ -6,17 +6,35 @@ import maceta from '../../images/maceta.svg'
 import regadera from '../../images/regadera.svg'
 import sol from '../../images/sol.svg'
 import abono from '../../images/abono.svg'
+import  {useParams}  from 'react-router-dom'
+import axios from 'axios';
 
 const DetallesPlantas = () => {
-    return (
-        <div className='containerPrincipal'>
-            <div className='detalleContainer'>
+    const [plants, setplants] = useState([]);
+    const { id } = useParams()
+    
+
+useEffect(()=> {
+axios.get('https://api.jsonbin.io/v3/b/63893d17003d6444ce6057d2')
+.then((res) => setplants(res.data.record.data.getPlants));
+window.scrollTo(0, 0)
+}, [])
+
+
+const resulPlants = () => {
+   const plantsfind = plants.find(plant => plant.id == id)
+
+   console.log(plantsfind);
+    return(
+        <div className='detalleContainer'>
                 <div className='mx-4 imgContenedor'>
-                    <img src={Albahaca} alt="" />
+                    <img
+                    className='img-fluid'
+                     src={plantsfind?.imagen} alt="" />
                 </div>
                 <div className='col-11 col-md-5 col-lg-6 mx-1 contenedorInfo'>
-                    <h3 className='mb-2 h3detalle'>Albahaca</h3>
-                    <p className='infoPlanta'>Plantar albahaca en casa no es para nada difícil. Podes cultivarla desde sus semillas o desde un esqueje, es decir, una rama cortada de otra planta. Luego de ello, solo necesitaremos una ventana donde la planta recibe suficiente sol.
+                    <h3 className='mb-2 h3detalle'>{plantsfind?.nombre}</h3>
+                    <p className='infoPlanta'>{plantsfind?.descripcion1} {plantsfind?.descripcion2}
                     </p>
                 </div>
                 <div className='contIcons '>
@@ -27,13 +45,21 @@ const DetallesPlantas = () => {
                         <img className='imgIcon' src={regadera} alt="" />
                     </div>
                     <div className='infoIconos'>
-                        <span>Cultivo en Otoño/invierno</span>
-                        <span>Siembra en primavera/verano</span>
-                        <span>Apto interior y exterior</span>
-                        <span>Nivel de exigencia intermedio</span>
+                        <span>{plantsfind?.cultivo}</span>
+                        <span>{plantsfind?.siembra}</span>
+                        <span>{plantsfind?.apto}</span>
+                        <span>{plantsfind?.exigencia}</span>
                     </div>
                 </div>
             </div>
+    )
+}
+    
+
+    return (
+        <div className='containerPrincipal'>
+            {resulPlants()}
+           
         </div>
     );
 }
